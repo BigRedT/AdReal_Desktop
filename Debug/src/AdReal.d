7448b75218108c884012bedb8f274fee4aac127f
tanmay <tgupta6@illinois.edu>
2015-07-25 20:04:02 -0500

    Kickstart

---
+++ Debug/src/AdReal.d
@@ -0,0 +1,1 @@
+src/AdReal.d: ../src/AdReal.cpp
---
+++ src/AdReal.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : AdReal.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+
+int main() {
+	cv::Mat img;
+	cv::namedWindow("Something",1);
+
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	return 0;
+}
