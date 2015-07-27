5980a6069374bd176c9e98dc8a9506daa83c9e44
tanmay <tgupta6@illinois.edu>
2015-07-26 20:34:01 -0500

    Modified tracker so that it at least compiles

--- includes/tracker.hpp
+++ includes/tracker.hpp
@@ -1,23 +1,37 @@
 #ifndef _TRACKER_HPP_
 #define _TRACKER_HPP_
 
+#include <vector>
 #include <opencv2/video.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/optflow.hpp>
+
 class Tracker {
 
 private:
-	cv::Size winSize = cv::Size(21,21);
-	int maxLevel = 3;
-	cv::TermCriteria criteria = cv::TermCriteria(cv::TermCriteria::COUNT+cv::TermCriteria::EPS, 30, 0.01);
-	int flags = 0;
-	double minEigThresh = 0;
+	int windowSize;
+	int maxLevel;
+	int maxIterations;
+	double epsilon;
+	int flags;
+	double minEigThresh;
+
 
 public:
-	const std::vector<cv::Point> &prevPts_;
-	std::vector<cv::Point> &currPts_;
-	std::vector<uchar> &trackStatus_;
+	std::vector<cv::Point> *currPts_;
+	std::vector<uchar> *trackStatus_;
 
 public:
-	void trackPoints(const cv::Mat &prevImg, const std::vector<cv::Point> &prevPts,
+	Tracker() {
+		windowSize = 21;
+		maxLevel = 3;
+		maxIterations = 30;
+		epsilon = 0.01;
+		flags = 0 + cv::OPTFLOW_LK_GET_MIN_EIGENVALS;
+		minEigThresh = 0;
+	}
+
+	void trackPoints(cv::Mat &prevImg, std::vector<cv::Point> &prevPts,
 					 cv::Mat &currImg, std::vector<cv::Point> &currPts, std::vector<uchar> &trackStatus);
 };
 
--- src/tracker.cpp
+++ src/tracker.cpp
@@ -1,11 +1,12 @@
 #include "tracker.hpp"
 
-void Tracker::trackPoints(const cv::Mat &prevImg, const std::vector<cv::Point> &prevPts,
+void Tracker::trackPoints(cv::Mat &prevImg, std::vector<cv::Point> &prevPts,
 		 	 	 	 	  cv::Mat &currImg, std::vector<cv::Point> &currPts, std::vector<uchar> &trackStatus) {
 	std::vector<double> err;
-	cv::calcOpticalFlowPyrLK(prevImg, currImg, prevPts, currPts, trackStatus, err,
-							 this->winSize, this->maxLevel, this->criteria, this->flags, this->minEigThresh);
-	this->prevPts_ = prevPts;
-	this->currPts_ = currPts;
-	this->trackStatus_ = trackStatus;
+	cv::Size winSize = cv::Size(this->windowSize, this->windowSize);
+	cv::TermCriteria criteria = cv::TermCriteria(cv::TermCriteria::COUNT+cv::TermCriteria::EPS, this->maxIterations, this->epsilon);
+	cv::calcOpticalFlowPyrLK(prevImg, currImg, prevPts, currPts, trackStatus, err, winSize, this->maxLevel, criteria, this->flags, this->minEigThresh);
+	this->currPts_ = &currPts;
+	this->trackStatus_ = &trackStatus;
+
 }
