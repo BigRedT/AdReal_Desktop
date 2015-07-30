60311aca51fbe6139f0ec9eeb10449adb088fffb
Wenda Wang <wwd7086@gmail.com>
2015-07-30 13:16:14 -0700

    tracking engine made

--- src/AdReal.cpp
+++ src/AdReal.cpp
@@ -1,18 +1,15 @@
 #include "test.hpp"
-#include "corner_detector.hpp"
-#include "tracker.hpp"
+#include "tracking_engine.hpp"
 #include <iostream>
 #include <vector>
-#include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/video/tracking.hpp>
 
 using namespace cv;
 using std::vector;
 
 int main() {
-	const std::string video_filename = "resource/video_simple.mp4";
 	// read video
+	const std::string video_filename = "resource/video_simple.mp4";
 	cv::VideoCapture capture(video_filename);
 	if (!capture.isOpened()) {
 		std::cout << "Video file could not be opened" << std::endl;
@@ -22,50 +19,23 @@ int main() {
 	// Create window to display video frames
 	cv::namedWindow("AdReal", WINDOW_AUTOSIZE);
 
-	// detector
-	CornerDetector detector;
-	// tracker
-	Tracker tracker;
-
-	// kalman filter
-	KalmanFilter KF(8, 8, 8);  //numState, numMeasure, numControl
-	setIdentity(KF.measurementMatrix); //C
-	setIdentity(KF.transitionMatrix);  //A
-	setIdentity(KF.controlMatrix); //B
-	KF.processNoiseCov = Mat::eye(8, 8, CV_32F) * 0.001;
-	KF.measurementNoiseCov = Mat::eye(8, 8, CV_32F) * 0.5;
-
-	Mat delta(8, 1, CV_32F);
-	Mat measure(8, 1, CV_32F);
-
-	// points
-	vector<Point2f> inPts;
-	vector<Point2f> outPts;
-	vector<uchar> status;
-
-	// init tracking point
-	inPts.push_back(Point2f(1016, 166));
-	inPts.push_back(Point2f(1480, 158));
-	inPts.push_back(Point2f(1470, 796));
-	inPts.push_back(Point2f(986, 774));
+	// Create tracking engine
+	TrackingEngine engine;
 
 	// Process each video frame
 	Mat frame_c, frame, preFrame_c, preFrame;
 	capture >> preFrame_c;
 	cvtColor(preFrame_c, preFrame, COLOR_BGR2GRAY);
 
-	// Refine the initial points
-	for (int i = 0; i < (int) (inPts.size()); i++) {
-		detector.detectCorner(preFrame, inPts[i], 10, inPts[i]);
-	}
+	// init tracking point
+	engine.addTrackingPoint(Point2f(1016, 166),preFrame);
+	engine.addTrackingPoint(Point2f(1480, 158),preFrame);
+	engine.addTrackingPoint(Point2f(1470, 796),preFrame);
+	engine.addTrackingPoint(Point2f(986, 774) ,preFrame);
 
-	// init kF state
-	for (int i = 0; i < (int) (inPts.size()); i++) {
-		KF.statePost.at<float>(2 * i, 0) = inPts[i].x;
-		KF.statePost.at<float>(2 * i + 1, 0) = inPts[i].y;
-	}
+	// output points
+	vector<Point2f> points;
 
-	int count = 0;
 	while (1) {
 		capture >> frame_c;
 		if (frame_c.empty()) {
@@ -74,40 +44,12 @@ int main() {
 		}
 		cvtColor(frame_c, frame, COLOR_BGR2GRAY);
 
-		if (count < 20) {
-			// run tracker
-			tracker.trackPoints(preFrame, inPts, frame, outPts, status);
-			for (int i = 0; i < (int) (inPts.size()); i++) {
-				delta.at<float>(i * 2, 0) = outPts[i].x - inPts[i].x;
-				delta.at<float>(i * 2 + 1, 0) = outPts[i].y - inPts[i].y;
-			}
-
-			// kalman predict
-			KF.predict(delta);
-			KF.statePost = KF.statePre;
-
-		} else {
-			// run detector
-			for (int i = 0; i < (int) (inPts.size()); i++) {
-				detector.detectCorner(frame, inPts[i], 10, outPts[i]);
-				measure.at<float>(i * 2, 0) = outPts[i].x;
-				measure.at<float>(i * 2 + 1, 0) = outPts[i].y;
-			}
-
-			// kalman corrector
-			KF.correct(measure);
-			count = 0;
-		}
-
-		// update the inpts
-		for (int i = 0; i < (int) (inPts.size()); i++) {
-			inPts[i].x = KF.statePost.at<float>(i * 2, 0);
-			inPts[i].y = KF.statePost.at<float>(i * 2 + 1, 0);
-		}
+		//Track!!
+		engine.trackAllPoints(frame,preFrame,points);
 
 		// visualize the point
-		for (int i = 0; i < (int) (inPts.size()); i++) {
-			circle(frame, inPts[i], 3, Scalar(0, 255, 0), 2, 8, 0);
+		for (int i = 0; i < (int) (points.size()); i++) {
+			circle(frame, points[i], 3, Scalar(0, 255, 0), 2, 8, 0);
 		}
 
 		// draw the frame
@@ -116,7 +58,6 @@ int main() {
 
 		// pre
 		preFrame = frame.clone();
-		count++;
 	}
 	return 0;
 }
---
+++ src/tracking_engine.cpp
@@ -0,0 +1,85 @@
+#include "tracking_engine.hpp"
+#include <iostream>
+
+using namespace cv;
+
+TrackingEngine::TrackingEngine() {
+
+	//init the kalman filter
+	KF = KalmanFilter(8, 8, 8);  //numState, numMeasure, numControl
+	setIdentity(KF.measurementMatrix); //C
+	setIdentity(KF.transitionMatrix);  //A
+	setIdentity(KF.controlMatrix); //B
+	KF.processNoiseCov = Mat::eye(8, 8, CV_32F) * 0.001;
+	KF.measurementNoiseCov = Mat::eye(8, 8, CV_32F) * 0.5;
+
+	//others
+	count = 0;
+	points.clear();
+}
+
+bool TrackingEngine::addTrackingPoint(const Point2f& point, const Mat& frame) {
+	if (points.size() >= 4)
+		return false;
+
+	// add to point repo
+	Point2f corrected;
+	detector.detectCorner(frame, point, 10, corrected);
+	points.push_back(corrected);
+
+	// init kalman state
+	int i = points.size() - 1;
+	KF.statePost.at<float>(2 * i, 0) = corrected.x;
+	KF.statePost.at<float>(2 * i + 1, 0) = corrected.y;
+
+	return true;
+}
+
+bool TrackingEngine::trackAllPoints(
+		const Mat& cFrame, const Mat& pFrame,
+		std::vector<Point2f>& outPoints)
+{
+	if (points.size() == 0)
+		return true;
+
+	std::vector<Point2f> newPoints = points;
+	std::vector<uchar> status;
+	outPoints = points;
+
+	Mat delta = Mat::zeros(cv::Size(1,8),CV_32F);
+	Mat measure = Mat::zeros(cv::Size(1,8),CV_32F);
+
+	if (count < 20) {
+		// run tracker
+		tracker.trackPoints(pFrame, points, cFrame, newPoints, status);
+		for (int i = 0; i < (int) (points.size()); i++) {
+			delta.at<float>(i * 2, 0) = newPoints[i].x - points[i].x;
+			delta.at<float>(i * 2 + 1, 0) = newPoints[i].y - points[i].y;
+		}
+
+		// kalman predict
+		KF.predict(delta);
+
+	} else {
+		// run detector
+		for (int i = 0; i < (int) (points.size()); i++) {
+			detector.detectCorner(cFrame, points[i], 10, newPoints[i]);
+			measure.at<float>(i * 2, 0) = newPoints[i].x;
+			measure.at<float>(i * 2 + 1, 0) = newPoints[i].y;
+		}
+
+		// kalman corrector
+		KF.correct(measure);
+		count = 0;
+	}
+
+	// update the inpts
+	for (int i = 0; i < (int) (points.size()); i++) {
+		points[i].x = KF.statePost.at<float>(i * 2, 0);
+		points[i].y = KF.statePost.at<float>(i * 2 + 1, 0);
+		outPoints[i].x = KF.statePost.at<float>(i * 2, 0);
+		outPoints[i].y = KF.statePost.at<float>(i * 2 + 1, 0);
+	}
+
+	return true;
+}
